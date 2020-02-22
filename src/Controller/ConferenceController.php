3a206c014e54c3d2ba740dbc47e803af527d13e8
Eduardo Ahumada <eddie.cpp@gmail.com>
2020-02-22 09:14:55 -0300

    Creando un sencillo controlador

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(Request $request)
+    {
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('Hello ' . htmlspecialchars($name));
+        }
+        return $this->render('conference/index.html.twig', [
+            'controller_name' => 'ConferenceController',
+            'greet' => $greet
+        ]);
+    }
+}
